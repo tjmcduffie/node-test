29dbd54377fd595e7ffb0c2c3a1e277c03d2bbe8
Tim McDuffie <timothy.mcduffie@gmail.com>
2017-07-07 19:18:36 -0400

    flow nits & fixed rendering bug

--- app/web/routes/BaseHtmlRoute.js
+++ app/web/routes/BaseHtmlRoute.js
@@ -6,32 +6,42 @@
 
 "use strict";
 
+import type {$Request, $Response} from 'express';
+import type {Element as ReactElement} from 'react';
+
 const BaseError = require('~/app/lib/BaseError');
 const DefaultContainer = require('~/app/web/components/layout/DefaultContainer');
 const React = require('react');
 
-import type {$Request, $Response} from 'express';
-import type {Element as ReactElement} from 'react';
-
 class BaseHtmlRoute {
-  _container: string;
+  _container: ReactElement<*>;
+  _layoutView: string;
   _pageTitle: string;
   _req: $Request;
   _res: $Response;
 
   constructor(req: $Request, res: $Response) {
     this._container = DefaultContainer;
+    this._layoutView = 'Default';
     this._pageTitle = 'Test Page';
     this._req = req;
     this._res = res;
   }
 
-  getLayoutView(): string {
+  getContainer(): string {
     return this._container;
   }
 
+  setContainer(container: ReactElement<*>): void {
+    this._container = container;
+  }
+
+  getLayoutView(): string {
+    return this._layoutView;
+  }
+
   setLayoutView(view: string): void {
-    this._container = view;
+    this._layoutView = view;
   }
 
   getPageTitle(): string {
@@ -47,21 +57,21 @@ class BaseHtmlRoute {
   }
 
   get(): void {
-    let pageContent: ?ReactElement<*> = null;
-    let children: ?Array<ReactElement<*>>|ReactElement<*> = null;
+    let pageContainer: ?ReactElement<*> = null;
+    let pageContent: ?Array<ReactElement<*>>|ReactElement<*> = null;
 
     try {
-      children = this.setDesktopResponse();
-      pageContent = React.createElement(this._container, {children});
+      pageContent = this.setDesktopResponse();
+      pageContainer = React.createElement(this.getContainer(), {pageContent});
     } catch (e) {
       this._res.sendStatus(500);
-      return;
+      throw e;
     }
 
     this._res
       .render(this.getLayoutView(), {
         title: this.getPageTitle(),
-        children: pageContent,
+        children: pageContainer,
       });
   }
 }
