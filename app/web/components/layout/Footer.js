24125c56740c5d250ef2042db9e782d72b94a0a2
Tim McDuffie <timothy.mcduffie@gmail.com>
2017-07-07 23:56:07 -0400

    modularized Footer css

--- app/web/components/layout/Footer.js
+++ app/web/components/layout/Footer.js
@@ -1,21 +1,27 @@
 /*global */
 /**
- * 
- * //flow
+ *
+ * @flow
  */
 
 "use strict";
 
+import type {Element as ReactElement} from 'react';
+
 const React = require('react');
 
-module.exports = function Footer(props: {
-  id: string,
-}) {
+const cx = require('classNames');
+const styles = require('~/app/static_src/css/Footer.css');
+
+const year = (new Date()).getFullYear();
+
+module.exports = function Footer(): ReactElement<*> {
   return (
-    <footer id={props.id}>
-      <ul className="block row">
-        <li className="lg-col-12">
-          All original content &copy; 2017 Tim McDuffie unless otherwise noted.
+    <footer className={cx(styles.root)}>
+      <ul className={cx(styles.list)}>
+        <li>
+          All original content &copy; {year} Tim McDuffie unless otherwise
+          noted.
         </li>
       </ul>
     </footer>
