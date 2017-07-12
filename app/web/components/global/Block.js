359d3b7abc52f146503c25ce12393d6c37eb1954
Tim McDuffie <timothy.mcduffie@gmail.com>
2017-07-11 21:11:29 -0400

    added generic block component

---
+++ app/web/components/global/Block.js
@@ -0,0 +1,65 @@
+/*global */
+/**
+ *
+ * @flow
+ */
+
+"use strict";
+
+import type {Element as ReactElement} from 'react';
+
+type BlockStyles = 'green'|'grey'|'gridDark'|'gridLight'|'none'|'white';
+
+type Props = {
+  children: ReactElement<*> | Array<ReactElement<*>>,
+  className?: string,
+  classNameInner?: string,
+  theme: BlockStyles,
+};
+
+const React = require('react');
+
+const cx = require('classNames');
+const style = require('~/app/static_src/css/Block.css');
+
+class Block extends React.PureComponent {
+  props: Props;
+
+  static theme: {[string]: BlockStyles} = {
+    GREEN: 'green',
+    GREY: 'grey',
+    GRID_DARK: 'gridDark',
+    GRID_LIGHT: 'gridLight',
+    NONE: 'none',
+    WHITE: 'white',
+  };
+
+  render(): ReactElement<*> {
+    const {
+      children,
+      className,
+      classNameInner,
+      theme,
+    } = this.props;
+
+    return (
+      <div
+        className={cx(className, {
+          [style.root]: true,
+          [style.green]: theme === Block.theme.GREEN,
+          [style.grey]: theme === Block.theme.GREY,
+          [style.gridDark]: theme === Block.theme.GRID_DARK,
+          [style.gridLight]: theme === Block.theme.GRID_LIGHT,
+          [style.none]: theme === Block.theme.NONE,
+          [style.white]: theme === Block.theme.WHITE,
+        })}
+      >
+        <div className={cx(style.inner, classNameInner)}>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Block;
